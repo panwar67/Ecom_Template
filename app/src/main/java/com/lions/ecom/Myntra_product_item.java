dfa20c45d1d9f65c3c7bd3d4b51a388042ef2419
nikhil-pandita <nikhil-pandita>
2017-07-17 17:38:22 +0530

    aha
    hahahaha

---
+++ app/src/main/java/com/lions/ecom/Myntra_product_item.java
@@ -0,0 +1,14 @@
+package com.lions.ecom;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class Myntra_product_item extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_myntra_product_item);
+    }
+}
